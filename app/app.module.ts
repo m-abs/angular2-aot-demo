a9ec08c93a37e81f99dece25e6f339166fcd1852
Morten Sjøgren <m_abs@mabs.dk>
2016-09-21 21:46:21 +0200

    Example with module built with ngc

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { MyModule } from 'project-name';
+
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MyModule],
   providers: []
 })
 export class AppModule {}
